29864e7bc18a8556ada28a0705310178d5aa98c1
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-06-24 17:33:50 -0400

    Home centered and imange fixed

--- Frontend/lib/screens/homepage.dart
+++ Frontend/lib/screens/homepage.dart
@@ -1,7 +1,6 @@
 import 'package:fab_circular_menu/fab_circular_menu.dart';
 import 'package:flutter/material.dart';
 
-import 'auth/sign_up.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,41 +13,40 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Container(
-          width: MediaQuery.of(context).size.width-40,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(20)),
-            image: DecorationImage(image: AssetImage("assets/image/beach.jpg"), fit: BoxFit.cover)
-          ),
-          child: Text("Welcome Back", style: Theme.of(context).textTheme.headline1,),
-        )
-      ],
-    ),
-    floatingActionButton: FabCircularMenu(
-      children: <Widget>[
+      body: Center(
+        child:
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            Container(
+              width: MediaQuery.of(context).size.width - 40,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/beach.jpg"),
+                      fit: BoxFit.cover)),
+              child: Text(
+                "Welcome Back",
+                style: Theme.of(context).textTheme.headline1,
+              ),
+            ),
+            Spacer(),
+          ],
+        ),
+      ),
+      floatingActionButton: FabCircularMenu(children: <Widget>[
         IconButton(
           icon: Icon(Icons.travel_explore),
           tooltip: "Create a new trip",
-          onPressed: (){
-
-          },
+          onPressed: () {},
         ),
         IconButton(
-          icon: Icon(Icons.add_location_alt),
-          tooltip: "Add a new activity",
-          onPressed: (){
-
-          }
-        )
-      ]
-    ),
+            icon: Icon(Icons.add_location_alt),
+            tooltip: "Add a new activity",
+            onPressed: () {})
+      ]),
     );
   }
 }
-
-
-
- 
